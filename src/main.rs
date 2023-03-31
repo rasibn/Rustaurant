4762e06d25e5d122d548f5468fd8bacfd349a07d
Rasib <rasibnadeem101@gmail.com>
2023-03-31 10:18:05 +0500

    Implemented a basic module system.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use axum::{
     routing::{get, post},
-    http::StatusCode,
     //response::IntoResponse,
-    Json, Router,
+    Router,
 };
-use serde::{Deserialize, Serialize};
+
+mod handlers;
+mod structs;
+
+use handlers::handlers::{create_user, root};
+
 use std::net::SocketAddr;
 
 #[tokio::main]
@@ -30,36 +34,3 @@ async fn main() {
         .unwrap();
 }
 
-// basic handler that responds with a static string
-async fn root() -> &'static str {
-    "Hello, World!"
-}
-
-async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>,
-) -> (StatusCode, Json<User>) {
-    // insert your application logic here
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(user))
-}
-
-// the input to our `create_user` handler
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
-}
-
-// the output to our `create_user` handler
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
-}
