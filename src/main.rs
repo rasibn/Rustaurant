69f28872b6673949823ba4f7d841f17a7f2fd0d3
Rasib <rasibnadeem101@gmail.com>
2023-04-02 15:51:40 +0500

    Added basic routes.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,62 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
+
+
+
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/")]
+    Home,
+    #[at("/about")]
+    About,
+    #[at("/secure")]
+    Secure,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+
+}
+
+#[function_component(Secure)]
+fn secure() -> Html {
+    let navigator = use_navigator().unwrap();
+
+    let onclick = Callback::from(move |_| navigator.push(&Route::Home));
+    html! {
+        <div>
+            <h1>{ "Secure" }</h1>
+            <button {onclick}>{ "Go Home" }</button>
+        </div>
+    }
+}
+
+#[function_component(Home)]
+pub fn home() -> Html {
+    html! { <h1>{"Welcome to my restaurant review app!"}</h1> }
+}
+
+#[function_component(About)]
+pub fn about() -> Html {
+    html! { <h1>{"About us"}</h1> }
+}
+
+fn switch(routes: Route) -> Html {
+    match routes {
+        Route::About => html! { <About /> },
+        Route::Home => html! { <Home />},
+        Route::Secure => html! {
+            <Secure />
+        },
+        Route::NotFound => html! { <h1>{ "404" }</h1> },
+    }
+}
 
 #[function_component(App)]
 fn app() -> Html {
     html! {
-        <h1>
-        <button>{ "Click me!" }</button>
-        { "Hello World" }</h1>
+        <BrowserRouter>
+            <Switch<Route> render={switch} /> // <- must be child of <BrowserRouter>
+        </BrowserRouter>
     }
 }
 
---
+++ src/tempCodeRunnerFile.rs
@@ -0,0 +1,2 @@
+
+    pub fn home() -> Html {
