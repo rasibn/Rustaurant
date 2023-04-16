be1014b1ce5aef2428def2d6d89d86f8c0c722cc
Rasib <rasibnadeem101@gmail.com>
2023-04-17 01:06:17 +0500

    fixed typo in footer

--- src/components/layout/footer.rs
+++ src/components/layout/footer.rs
@@ -27,7 +27,7 @@ pub fn footer() -> Html {
             </ul>
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">{"© 2023 "}<a href="https://google.com/" class="hover:underline">{"™"}</a>{". All Rights Reserved."}</span>
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">{"© 2023 "}<a href="https://google.com/" class="hover:underline">{"Bitesy™"}</a>{". All Rights Reserved."}</span>
     </div>
 </footer>
   }
