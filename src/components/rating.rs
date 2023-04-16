d9b98c5ef4866bdc80b17071ada0d100a75a702c
Rasib <rasibnadeem101@gmail.com>
2023-04-17 00:35:26 +0500

    added props to rating component

--- src/components/rating.rs
+++ src/components/rating.rs
@@ -1,52 +1,60 @@
-use yew::{function_component, html, Html};
+use yew::{function_component, html, Html, Properties};
+
+#[derive(Properties, PartialEq)]
+pub struct Props {
+    pub is_loading: bool,
+    pub num_star: [i32; 5],
+    pub restaurant_name: String,
+    pub restaurant_description: String,
+    pub restaurant_address: String,
+}
 
 #[function_component(Rating)]
-pub fn rating() -> Html {
-    html! { 
+pub fn rating(props: &Props) -> Html {
+    //panic!("num_star: {:?}", props.num_star);
+    let num_reviews = props.num_star.iter().fold(0, |acc, &num| acc + num);
+    let avg_rating = props.num_star.iter().enumerate().fold(0.0, |acc, (i, &num)| {
+        acc + (i as f32 + 1.0) * num as f32
+    }) / num_reviews as f32;
+
+    let avg_rating = (avg_rating*100.0).round()/100.0;
+    let avg_rating_int = avg_rating as i32;
+    
+    let star_class: [String; 5] = [
+        if avg_rating_int > 0 { "w-5 h-5 text-yellow-400" } else { "w-5 h-5 text-gray-300 dark:text-gray-500" }.to_string(),
+        if avg_rating_int > 1 { "w-5 h-5 text-yellow-400" } else { "w-5 h-5 text-gray-300 dark:text-gray-500" }.to_string(),
+        if avg_rating_int > 2 { "w-5 h-5 text-yellow-400" } else { "w-5 h-5 text-gray-300 dark:text-gray-500" }.to_string(),
+        if avg_rating_int > 3 { "w-5 h-5 text-yellow-400" } else { "w-5 h-5 text-gray-300 dark:text-gray-500" }.to_string(),
+        if avg_rating_int > 4 { "w-5 h-5 text-yellow-400" } else { "w-5 h-5 text-gray-300 dark:text-gray-500" }.to_string()];
+
+    html! {
     <div>
     <div class="flex items-center mb-3">
-        <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>{"First star"}</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-        <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>{"Second star"}</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-        <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>{"Third star"}</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-        <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>{"Fourth star"}</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-        <svg aria-hidden="true" class="w-5 h-5 text-gray-300 dark:text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>{"Fifth star"}</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-        <p class="ml-2 text-sm font-medium text-gray-900 dark:text-white">{"4.95 out of 5"}</p>
+        <svg aria-hidden="true" class={&star_class[0]} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>{"First star"}</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+        <svg aria-hidden="true" class={&star_class[1]} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>{"Second star"}</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+        <svg aria-hidden="true" class={&star_class[2]} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>{"Third star"}</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+        <svg aria-hidden="true" class={&star_class[3]} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>{"Fourth star"}</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+        <svg aria-hidden="true" class={&star_class[4]} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>{"Fifth star"}</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+        
+        <p class="ml-2 text-sm font-medium text-gray-900 dark:text-white">{avg_rating}{" out of 5"}</p>
     </div>
-    <p class="text-sm font-medium text-gray-500 dark:text-gray-400">{"1,745 global ratings"}</p>
-    <div class="flex items-center mt-4">
-        <span class="text-sm font-medium text-blue-600 dark:text-blue-500">{"5 star"}</span>
-        <div class="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
-            <div class="h-5 bg-yellow-400 rounded" style="width: 70%"></div>
-        </div>
-        <span class="text-sm font-medium text-blue-600 dark:text-blue-500">{"70%"}</span>
-    </div>
-    <div class="flex items-center mt-4">
-        <span class="text-sm font-medium text-blue-600 dark:text-blue-500">{"4 star"}</span>
-        <div class="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
-            <div class="h-5 bg-yellow-400 rounded" style="width: 17%"></div>
-        </div>
-        <span class="text-sm font-medium text-blue-600 dark:text-blue-500">{"17%"}</span>
-    </div>
-    <div class="flex items-center mt-4">
-        <span class="text-sm font-medium text-blue-600 dark:text-blue-500">{"3 star"}</span>
-        <div class="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
-            <div class="h-5 bg-yellow-400 rounded" style="width: 8%"></div>
-        </div>
-        <span class="text-sm font-medium text-blue-600 dark:text-blue-500">{"8%"}</span>
-    </div>
-    <div class="flex items-center mt-4">
-        <span class="text-sm font-medium text-blue-600 dark:text-blue-500">{"2 star"}</span>
-        <div class="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
-            <div class="h-5 bg-yellow-400 rounded" style="width: 4%"></div>
-        </div>
-        <span class="text-sm font-medium text-blue-600 dark:text-blue-500">{"4%"}</span>
-    </div>
-    <div class="flex items-center mt-4">
-        <span class="text-sm font-medium text-blue-600 dark:text-blue-500">{"1 star"}</span>
-        <div class="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
-            <div class="h-5 bg-yellow-400 rounded" style="width: 1%"></div>
-        </div>
-        <span class="text-sm font-medium text-blue-600 dark:text-blue-500">{"1%"}</span>
-    </div>
+    <p class="text-sm font-medium text-gray-500 dark:text-gray-400">{num_reviews}{" global ratings"}</p>
+    {
+       props.num_star.iter().enumerate().map(|tuple| {
+        let (i, num_star) = tuple;
+        let rating_percent = (*num_star as f32 / num_reviews as f32) * 100.0;
+        let rating_percent_int = rating_percent.round() as i32;            
+        html! {
+                <div class="flex items-center mt-4">
+                <span class="text-sm font-medium text-blue-600 dark:text-blue-500">{i+1}{" star"}</span>
+                <div class="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+                    <div class="h-5 bg-yellow-400 rounded" style={format!("width: {}%", rating_percent_int)}></div>
+                </div>
+                <span class="text-sm font-medium text-blue-600 dark:text-blue-500">{rating_percent_int}{"%"}</span>
+                </div>
+            }
+        }).collect::<Html>()
+
+    }
     </div>}
 }
--- src/pages/restaurant.rs
+++ src/pages/restaurant.rs
@@ -3,15 +3,20 @@ use crate::components::review::Review;
 use crate::components::rating::Rating;
 
 use yew::prelude::*;
-// TODO: Add props
+// TODO: fix is_loading
 #[function_component(Restaurant)]
 pub fn restaurant() -> Html {
+
     html! { 
         <Layout>
             <h1 class="mb-2 mt-0 text-5xl font-medium leading-tight text-primary">{ "Restuarant A" }</h1>  
             <p class={classes!("")}> {" "}</p> 
             <br/> 
-            <Rating/>
+            <Rating is_loading = {false}
+                    num_star= {[50, 30, 13, 33, 52]}
+                    restaurant_name= {"Restaurant A".to_string()}
+                    restaurant_description= {"Restaurant A is a restaurant".to_string()}
+                    restaurant_address= {"123 Main St".to_string()}/>
             <br/>
             <h3 class="mb-2 mt-0 text-3xl font-medium leading-tight text-primary">{"Reviews"}</h3>
             <Review/>
